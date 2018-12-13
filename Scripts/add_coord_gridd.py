0bd285a3c6fccdd5f68da3ffa3cf11dd333fd48b
isgiddy <isgiddy@gmail.com>
2018-12-13 09:52:56 +0100

    script to convert asc to nc

--- Scripts/add_coord_gridd.py
+++ Scripts/add_coord_gridd.py
@@ -32,7 +32,7 @@ def sort_coords(datadir):
     for filename in ls:
         if filename[-2:] == 'nc':
             data =  xr.open_dataset(os.path.join(datadir,filename))
-            data.PRES.rename({'DEPTH',inplace=True})
+            data.rename({'PRES':'DEPTH'},inplace=True)
             data.swap_dims({'scan':'DEPTH'},inplace=True)            
             print('Moving {0} to {0}.bak'.format(filename))
             os.rename(os.path.join(datadir,filename), \
@@ -40,12 +40,26 @@ def sort_coords(datadir):
             print('Saving file {}'.format(filename))
             data.to_netcdf(os.path.join(datadir,filename))
 
-def read_acsii(datadir):
-    data = pd.read_csv(datadir),sep='t/', 
-                  df=pd.read_csv('SK_20181210_04.asc')
-                  df=pd.DataFrame(df,columns=['timeS','lon','lat','TEMP','PRES','Flr','density',
-                  'depth','o2','psal'])
-                  df=pd.read_csv('SK_20181210_04.asc',columns=np.arange(10))
+def read_ascii(datadir,filename):
+    df=pd.read_csv(os.path.join(datadir,filename),sep='\s+',header=None)
+    df.columns=['timeS','lon','lat','TEMP','PRES','Flr','density',
+               'depth','o2','psal']
+    return df
+    
+def save_ascii2nc(datadir):
+    ls = os.listdir(datadir)
+    ls.sort()
+    for filename in ls:
+        if filename[-3:] == 'asc':
+            data=read_ascii(datadir,filename)
+            ds=data.to_xarray()
+            filename=filename[:-3]+'nc'
+            print('Saving file {}'.format(filename))
+            # raise(NotImplementedError)
+            ds.to_netcdf(os.path.join(datadir,filename))
+
+
+
 
 def gridd(datadir, MAXDEPTH=120):
     """
@@ -56,4 +70,4 @@ def gridd(datadir, MAXDEPTH=120):
 if __name__ == '__main__':
     #Has been done
     #add_coordinates('Data/ctd_files/', 'Data/Trygve/TB20181210_meta_edit.csv')
-    gridd('Data/ctd_files/')
+    save_ascii2nc('/home/wizard/Documents/observing_the_ocean/analysis/Data/Skagerak/SK20181210/SK20181210_CTD/SK20181210_Processed_data/')
