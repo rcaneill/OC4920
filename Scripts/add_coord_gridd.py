57f8c2cd39e1c490c8b29aa36cfc26b152bcb8b1
Romain <romain.caneill@yahoo.fr>
2018-12-14 14:10:32 +0100

    gridding data, removing lat/lon bugs

--- Scripts/add_coord_gridd.py
+++ Scripts/add_coord_gridd.py
@@ -14,13 +14,14 @@ def add_coordinates(datadir, metafilename):
         if filename[-2:] == 'nc':
             data =  xr.open_dataset(os.path.join(datadir,filename))
             meta_data = meta[meta['filename'] == filename]
-            data['lat'] = ('scan',np.ones(data.DEPTH.shape) * meta_data.lat.values[0])
-            data['lon'] = ('scan',np.ones(data.DEPTH.shape) * meta_data.lon.values[0])
-            print('Moving {0} to {0}.bak'.format(filename))
-            os.rename(os.path.join(datadir,filename), \
-                      os.path.join(datadir,filename+'.bak'))
-            print('Saving file {}'.format(filename))
-            data.to_netcdf(os.path.join(datadir,filename))
+            if not meta_data.empty:
+                data['lat'] = ('scan',np.ones(data.DEPTH.shape) * meta_data.lat.values[0])
+                data['lon'] = ('scan',np.ones(data.DEPTH.shape) * meta_data.lon.values[0])
+                print('Moving {0} to {0}.bak'.format(filename))
+                os.rename(os.path.join(datadir,filename), \
+                          os.path.join(datadir,filename+'.bak'))
+                print('Saving file {}'.format(filename))
+                data.to_netcdf(os.path.join(datadir,filename))
 
 def sort_coords(datadir):
     """
@@ -109,31 +110,55 @@ def gridd(datadir, filename, MAXDEPTH=120):
     #raise(NotImplementedError)
     data =  xr.open_dataset(os.path.join(datadir,filename))
     datanew = data.copy()
-    datanew = datanew.drop('DEPTH')
-    if 'LATITUDE' in str(data.data_vars.keys):
+    vars_and_coords = [i for i in datanew.data_vars.keys()] + \
+                      [i for i in datanew.coords.keys()]
+    if 'DEPTH' in vars_and_coords:
+        datanew = datanew.drop('DEPTH')
+        print('Removing DEPTH')
+    if 'LATITUDE' in vars_and_coords:
         datanew = datanew.rename({'LATITUDE':'lat'})
-    if 'LONGITUDE' in str(data.data_vars.keys):
+    if 'LONGITUDE' in vars_and_coords:
         datanew = datanew.rename({'LONGITUDE':'lon'})
+    #print(datanew)
+    for i in datanew.coords.keys():
+        # changing coordinates to variables
+        line = datanew[i]
+        datanew = datanew.drop(i)
+        datanew[i] = ('DEPTH', line)
     for old_str in ['scan', 'index']:
-        if old_str in str(data.data_vars.keys):
+        if old_str in datanew.dims:
             datanew=datanew.rename({old_str:'DEPTH'})
+            print('renaming scan/index')
     data_maxd = data.DEPTH.max()
     data_mind = data.DEPTH.min()
     if MAXDEPTH < data_maxd:
         print('Skiping data, depth is higher than MAXDEPTH')
         return 0
     datanew=datanew.rename({'DEPTH':'DEPTH_old'})
-    keys_tot = [i for i in datanew.keys() if i!='DEPTH_old']
+    #print([i for i in datanew.keys()])
+    #print(datanew)
+    vars_and_coords = [i for i in datanew.data_vars.keys()] + \
+                      [i for i in datanew.coords.keys()]
+    keys_tot = [i for i in vars_and_coords if i!='DEPTH_old']
     for key in keys_tot:
         line = data[key]
         line_new = np.zeros(MAXDEPTH) * np.NaN
         line_new[int(data_mind):line.shape[0]+int(data_mind)] = line
         datanew[key] = ('DEPTH', line_new)
+        #print(key)
+    #raise NotImplementedError
+    vars_and_coords = [i for i in datanew.data_vars.keys()] + \
+                      [i for i in datanew.coords.keys()]
+    #print(vars_and_coords)
     #print(datanew)
+    if 'DEPTH_old' in vars_and_coords:
+        datanew = datanew.drop('DEPTH_old')
+        print('Removing DEPTH_old')
+    #print(datanew.DEPTH)
     datanew.to_netcdf(os.path.join(datadir, filename[:-3]+'_grid.nc'))
     
 if __name__ == '__main__':
     #Has been done
-    #add_coordinates('Data/ctd_files/', 'Data/Trygve/TB20181210_meta_edit.csv')
+    #add_coordinates('Data/ctd_files/processed2nc','Data/ctd_files/meta/TB20181210_meta.csv')
     #save_ascii2nc('Data/Skagerak/SK20181210/SK20181210_CTD/SK20181210_Processed_data')
-    gridd_all('.')
+    gridd_all('Data/ctd_files/processed2nc/Trygve')
